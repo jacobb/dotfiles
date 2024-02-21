596740445fcf6d210b7f47d21438dfe5857da7f3
Jacob Burch-Hill <jacob@burch-hill.com>
2024-02-21 15:30:31 -0600

    add zsh config

---
+++ .config/zsh/aliases.sh
@@ -0,0 +1,23 @@
+alias cl='clear'
+alias l='ls -l'
+alias ll='ls -la'
+alias pjson='python -m json.tool'
+alias diepyc='find . -name "*.pyc" -exec rm -rf {} \;'
+alias flushdns='sudo killall -INFO mDNSResponder'
+alias d='docker'
+alias dc='docker-compose'
+alias ssh="TERM=xterm-256color /usr/bin/ssh"
+alias mutt="cd ~; neomutt"
+
+alias gr='cd $(git rev-parse --show-toplevel)'
+alias vwd='ink_fzf'
+
+alias cat='bat'
+alias j='just'
+alias g='git'
+
+# one day i'll pick
+alias vi='nvim'
+alias vim='nvim'
+
+alias bad_docker_volumes='comm -2 -3 <(docker volume ls -q | sort) <(docker volume ls -q -f name=postgres -f name=search| sort)'
---
+++ .config/zsh/prompt.sh
@@ -0,0 +1,3 @@
+autoload -U promptinit; promptinit
+[[ ! -f ~/.zsh/p10k.zsh ]] || source ~/.zsh/p10k.zsh
+source /opt/homebrew/share/powerlevel10k/powerlevel10k.zsh-theme
