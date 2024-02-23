d351937f6821ea8d51af7f00e9ffe345d2566457
Jacob Burch-Hill <jacob@burch-hill.com>
2024-02-23 17:00:03 -0600

    add quick app switching to hammerspoon

--- .config/hammerspoon/init.lua
+++ .config/hammerspoon/init.lua
@@ -1,3 +1,10 @@
--- Empty
 require('win')
 -- require('keycastr')
+
+hs.hotkey.bind({ "cmd", "ctrl" }, "1", function()
+  hs.application.find("WezTerm"):activate()
+end)
+
+hs.hotkey.bind({ "cmd", "ctrl" }, "2", function()
+  hs.application.find("Firefox Developer Edition"):activate()
+end)
